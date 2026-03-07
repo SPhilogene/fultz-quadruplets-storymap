5dae0a6f93428d05cea18893f014d81f442960c8
SPhilogene <95000285+SPhilogene@users.noreply.github.com>
2026-03-06 22:12:07 -0500

    Update google-doc-url.js
    update custom links

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1oRSNyFsLdbubD9_4tVYqm7ArKohOuKCK8Vw_LWU7H0w/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyAfd2IGjsmFFvWnG3o3QZuOLb5TOi6KwVg';
