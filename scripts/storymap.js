4c93d80ed89064d4c4b1ef379392ae1691d698aa
SPhilogene <95000285+SPhilogene@users.noreply.github.com>
2026-03-07 17:11:33 -0500

    Update storymap.js
    resize adinkra

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -143,7 +143,7 @@ $(window).on('load', function() {
     // custom icon for markers
 var customIcon = L.icon({
     iconUrl: 'media/sankofa-small.png',
-    iconSize:     [38, 35], // size of the icon
+    iconSize:     [60, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
